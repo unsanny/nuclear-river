0802fb570036b498989873085a018550770a569e
a.rechkalov <a.rechkalov@2gis.ru>
2016-02-18 16:19:05 +0600

    Улучшил регистрацию clr-типов в контексте
    Вместо пространства имён используется информация из ISubDomain

---
+++ CustomerIntelligence/Tests/CustomerIntelligence.Replication.DataTest.Model/SchemaMetadataElementBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+using NuClear.DataTest.Metamodel;
+using NuClear.Model.Common.Entities;
+
+namespace NuClear.CustomerIntelligence.Replication.StateInitialization.Tests
+{
+    // todo: перенести в 2GIS.NuClear.DataTest.Metamodel
+    internal static class SchemaMetadataElementBuilderExtensions
+    {
+        public static SchemaMetadataElementBuilder HasEntitiesFromRegistry<TSubDomain>(this SchemaMetadataElementBuilder builder, IEntityTypeMappingRegistry<TSubDomain> registry)
+            where TSubDomain : ISubDomain
+        {
+            foreach (var type in registry.EntityMapping.Values.Union(registry.PersistenceEntities))
+            {
+                builder.WithFeatures(new EntityTypeFeature(type));
+            }
+
+            return builder;
+        }
+    }
+}
--- CustomerIntelligence/Tests/CustomerIntelligence.Replication.DataTest.Model/SchemaMetadataSource.cs
+++ CustomerIntelligence/Tests/CustomerIntelligence.Replication.DataTest.Model/SchemaMetadataSource.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using NuClear.CustomerIntelligence.Domain.Contexts;
 using NuClear.CustomerIntelligence.Replication.StateInitialization.Tests.Identitites.Connections;
 using NuClear.CustomerIntelligence.Storage;
 using NuClear.DataTest.Metamodel;
@@ -9,12 +10,6 @@ using NuClear.Metamodeling.Provider.Sources;
 
 namespace NuClear.CustomerIntelligence.Replication.StateInitialization.Tests
 {
-    using Bit = NuClear.CustomerIntelligence.Domain.Model.Bit;
-    using CI = NuClear.CustomerIntelligence.Domain.Model.CI;
-    using Erm = NuClear.CustomerIntelligence.Domain.Model.Erm;
-    using Facts = NuClear.CustomerIntelligence.Domain.Model.Facts;
-    using Statistics = NuClear.CustomerIntelligence.Domain.Model.Statistics;
-
     public class SchemaMetadataSource : MetadataSourceBase<SchemaMetadataIdentity>
     {
         private static readonly SchemaMetadataElement Erm = SchemaMetadataElement.Config
@@ -22,31 +17,31 @@ namespace NuClear.CustomerIntelligence.Replication.StateInitialization.Tests
             .HasConnectionString<ErmTestConnectionStringIdentity>()
             .HasMasterConnectionString<ErmMasterConnectionStringIdentity>()
             .HasSchema(Schema.Erm)
-            .HasEntitiesFromNamespace(typeof(Erm::Account).Namespace);
+            .HasEntitiesFromRegistry(EntityTypeMap.CreateErmContext());
 
         private static readonly SchemaMetadataElement Facts = SchemaMetadataElement.Config
             .For(ContextName.Facts)
             .HasConnectionString<FactsTestConnectionStringIdentity>()
             .HasSchema(Schema.Facts)
-            .HasEntitiesFromNamespace(typeof(Facts::Account).Namespace);
+            .HasEntitiesFromRegistry(EntityTypeMap.CreateFactsContext());
 
         private static readonly SchemaMetadataElement CustomerIntelligence = SchemaMetadataElement.Config
             .For(ContextName.CustomerIntelligence)
             .HasConnectionString<CustomerIntelligenceTestConnectionStringIdentity>()
             .HasSchema(Schema.CustomerIntelligence)
-            .HasEntitiesFromNamespace(typeof(CI::CategoryGroup).Namespace);
+            .HasEntitiesFromRegistry(EntityTypeMap.CreateCustomerIntelligenceContext());
 
         private static readonly SchemaMetadataElement Bit = SchemaMetadataElement.Config
             .For(ContextName.Bit)
             .HasConnectionString<BitTestConnectionStringIdentity>()
             .HasSchema(Schema.Bit)
-            .HasEntitiesFromNamespace(typeof(Bit::FirmCategoryStatistics).Namespace);
+            .HasEntitiesFromRegistry(EntityTypeMap.CreateBitFactsContext());
 
         private static readonly SchemaMetadataElement Statistics = SchemaMetadataElement.Config
             .For(ContextName.Statistics)
             .HasConnectionString<StatisticsTestConnectionStringIdentity>()
             .HasSchema(Schema.Statistics)
-            .HasEntitiesFromNamespace(typeof(Statistics::FirmCategory3).Namespace);
+            .HasEntitiesFromRegistry(EntityTypeMap.CreateStatisticsContext());
 
         public SchemaMetadataSource()
         {
